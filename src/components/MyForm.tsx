5bec593a92125fb96d92f8eb80d5e339ce2ef3d3
dolevoper <omerdolev90@gmail.com>
2025-06-29 20:48:25 +0300

    use reducer

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, type FormEvent } from "react";
+import { useEffect, useReducer, useRef, useState, type FormEvent } from "react";
 import styles from "./MyForm.module.scss";
 
 type MyFormProps = {
@@ -79,35 +79,77 @@ export function MyForm({ onCancelClick }: MyFormProps) {
                     <label htmlFor="bio">Bio</label>
                     <textarea className={styles.input} name="bio" id="bio" rows={5}></textarea>
                 </div>
-                <article>
-                    <h3>Stats</h3>
-                    <div className={styles.inlineFormField}>
-                        <label htmlFor="strength">Strength</label>
-                        <input className={styles.input} type="number" id="strength" name="strength" defaultValue={0} />
-                    </div>
-                    <div className={styles.inlineFormField}>
-                        <label htmlFor="intelligence">Intelligence</label>
-                        <input className={styles.input} type="number" id="intelligence" name="intelligence" defaultValue={0} />
-                    </div>
-                    <div className={styles.inlineFormField}>
-                        <label htmlFor="agility">Agility</label>
-                        <input className={styles.input} type="number" id="agility" name="agility" defaultValue={0} />
-                    </div>
-                    <div className={styles.inlineFormField}>
-                        <label htmlFor="dexterity">Dexterity</label>
-                        <input className={styles.input} type="number" id="dexterity" name="dexterity" defaultValue={0} />
-                    </div>
-                    <div className={styles.inlineFormField}>
-                        <label htmlFor="luck">Luck</label>
-                        <input className={styles.input} type="number" id="luck" name="luck" defaultValue={0} />
-                    </div>
-                    <p>Remaining points: <span>35</span></p>
-                </article>
+                <CharacterStats />
                 <div className={styles.formActions}>
                     <button className={styles.primaryButton}>Create</button>
                     <button className={styles.secondaryButton} onClick={cancel}>Cancel</button>
                 </div>
             </form>
         </>
+    );
+}
+
+const pointsToDistribute = 35;
+
+type Stat = "strength" | "intelligence" | "agility" | "dexterity" | "luck";
+type Distribution = Record<Stat, number>;
+type Action = { stat: Stat, newValue: number };
+
+
+function isValidDistribution(stats: Record<Stat, number>) {
+    return stats.agility + stats.dexterity + stats.intelligence + stats.luck + stats.strength <= pointsToDistribute;
+}
+
+function updateDistribution(distribution: Distribution, action: Action): Distribution {
+    const updatedDistribution: Distribution = {
+        ...distribution,
+        [action.stat]: action.newValue,
+    };
+
+    return isValidDistribution(updatedDistribution) ? updatedDistribution : distribution;
+}
+
+const startingDistribution: Distribution = {
+    strength: 5,
+    intelligence: 5,
+    agility: 5,
+    dexterity: 5,
+    luck: 5
+};
+
+function CharacterStats() {
+    const [distribution, dispatch] = useReducer(updateDistribution, startingDistribution);
+
+    const remainingPoints = pointsToDistribute - (distribution.strength + distribution.intelligence + distribution.agility + distribution.dexterity + distribution.luck);
+
+    const updateStat = (stat: Stat) => (e: FormEvent<HTMLInputElement>) => {
+        dispatch({ stat, newValue: e.currentTarget.valueAsNumber ?? 0 });
+    };
+
+    return (
+        <article>
+            <h3>Stats</h3>
+            <div className={styles.inlineFormField}>
+                <label htmlFor="strength">Strength</label>
+                <input className={styles.input} type="number" min={0} id="strength" name="strength" value={distribution.strength} onInput={updateStat("strength")} />
+            </div>
+            <div className={styles.inlineFormField}>
+                <label htmlFor="intelligence">Intelligence</label>
+                <input className={styles.input} type="number" min={0} id="intelligence" name="intelligence" value={distribution.intelligence} onInput={updateStat("intelligence")} />
+            </div>
+            <div className={styles.inlineFormField}>
+                <label htmlFor="agility">Agility</label>
+                <input className={styles.input} type="number" min={0} id="agility" name="agility" value={distribution.agility} onInput={updateStat("agility")} />
+            </div>
+            <div className={styles.inlineFormField}>
+                <label htmlFor="dexterity">Dexterity</label>
+                <input className={styles.input} type="number" min={0} id="dexterity" name="dexterity" value={distribution.dexterity} onInput={updateStat("dexterity")} />
+            </div>
+            <div className={styles.inlineFormField}>
+                <label htmlFor="luck">Luck</label>
+                <input className={styles.input} type="number" min={0} id="luck" name="luck" value={distribution.luck} onInput={updateStat("luck")} />
+            </div>
+            <p>Remaining points: <span>{remainingPoints}</span></p>
+        </article>
     );
 }
