0e42cf42af9f976d56853f4ef0c518742587a02c
dolevoper <omerdolev90@gmail.com>
2025-07-02 19:56:37 +0300

    show and hide cursor track cursor position

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,19 +1,33 @@
 import { useReducer, type KeyboardEvent } from "react";
 import styles from "./RichTextEditor.module.scss";
 
-type State = string;
+type State = {
+    text: string,
+    cursorPosition: number,
+};
 type Action =
     | { type: "append", value: string }
     | { type: "backspace" };
 
 function reducer(state: State, action: Action): State {
     switch (action.type) {
-        case "append": return state + action.value;
-        case "backspace": return state.slice(0, -1);
+        case "append": return {
+            ...state,
+            text: state.text + action.value,
+            cursorPosition: state.cursorPosition + 1,
+        };
+        case "backspace": return {
+            ...state,
+            text: state.text.slice(0, -1),
+            cursorPosition: Math.max(0, state.cursorPosition - 1),
+        };
     }
 }
 
-const initialState: State = "Rich text editor";
+const initialState: State = {
+    text: "",
+    cursorPosition: 0,
+};
 
 export function RichTextEditor() {
     const [state, dispatch] = useReducer(
@@ -26,7 +40,7 @@ export function RichTextEditor() {
         //  * Show cursor only when focused
         //  * Move cursor back and forward with left and right arrow keys
         //  * Always keep cursor in view
-        console.log(e.key);
+        // console.log(e.key);
         
         if (e.key.length === 1) {
             e.preventDefault();
@@ -49,9 +63,11 @@ export function RichTextEditor() {
         }
     }
 
+    console.log(state.cursorPosition);
+
     return (
         <div className={styles.container} tabIndex={0} onKeyDown={handleKeyDown}>
-            {state}<span className={styles.cursor}>|</span>
+            {state.text}<span className={styles.cursor}>|</span>
         </div>
     );
 }
